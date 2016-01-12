2ecee77b39d070cb613076e4d7a9b70cc01e97e0
Benedikt Kristinsson <benedikt@inventati.org>
2016-01-12 19:50:06 +0000

    Only run one instance (or from VS debugger)

--- client0.2/Program.cs
+++ client0.2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace lokunclient
 {
@@ -14,9 +16,20 @@ namespace lokunclient
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new lokunclientform());
+            var process_name = Path.GetFileNameWithoutExtension(Application.ExecutablePath);
+            var instance_count = Process.GetProcessesByName(process_name).Length;
+            if (instance_count == 1 || Debugger.IsAttached)
+            {
+                // Hey, it's just me around here!
+                File.WriteAllBytes("c:\\users\\benedikt\\Desktop\\test.txt", new byte[] { (byte)'h', (byte)'a', (byte)'l', (byte)'l', (byte)'o' });
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new lokunclientform());
+            }
+            else
+            {
+                // do nothing
+            }
         }
     }
 }
